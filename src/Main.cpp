6990b6e0f5bfe1dad899a27213ee9a312fc3cb7f
Cashion <danielacashion@gmail.com>
2026-06-28 19:26:57 -0500

    had to update SOL to compile using clang
    also updated the make file to include all new libs and includes

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,8 +3,16 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include<imgui/imgui.h>
+#include<sol/sol.hpp>
+
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
+    glm::vec2 velocity = glm::vec2(5.0, -2.0);
+    auto _ = velocity;
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }

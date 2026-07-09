ad2dca9f668017eb51279877e2769f0b5fb63184
Cashion <danielacashion@gmail.com>
2026-07-08 20:32:46 -0500

    also grabbing missed h and cpp

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,6 +1,9 @@
 #include "ECS.h"
 #include <algorithm>
 #include <string>
+#include <utility>
+
+int IComponent::nextId = 0;
 
 int Entity::GetId() const { return this->_id; }
 
@@ -28,12 +31,49 @@ const Signature &System::GetComponentSignature() const {
 
 Entity Registry::CreateEntity() {
   //
-  int entityId = numEntities++;
-  if (entityId >= this->_entitiyComponentSignatures.size()) {
-    this->_entitiyComponentSignatures.resize(entityId + 1);
+  int entityId = _numEntities++;
+  if (entityId >= this->_entityComponentSignatures.size()) {
+    this->_entityComponentSignatures.resize(entityId + 1);
   }
   Entity entity(entityId);
-  entitiesToBeAdded.insert(entity);
+  this->_entitiesToBeAdded.insert(entity);
   Logger::Log("Entity created with id: " + std::to_string(entityId));
   return entity;
+};
+
+template <typename TComponent, typename... TArgs>
+void Registry::AddComponent(Entity entity, TArgs &&...args) {
+  const auto componentId = Component<TComponent>::GetId();
+  const auto entityId = entity.GetId();
+  if (componentId >= this->_componentPools.size()) {
+    this->_componentPools.resize(componentId + 1, nullptr);
+  }
+
+  if (!this->_componentPools[componentId]) {
+    Pool<TComponent> *newComponentPool = new Pool<TComponent>;
+    this->_componentPools[componentId] = newComponentPool;
+  }
+  Pool<TComponent> *componentPool = this->_componentPools[componentId];
+
+  if (entityId >= componentPool->GetSize()) {
+    componentPool->Resize(this->_numEntities);
+  }
+
+  TComponent newComponent(std::forward<TArgs>(args)...);
+  componentPool->Set(entityId, newComponent);
+  this->_entityComponentSignatures[entityId].set(componentId);
+};
+
+template <typename TComponent> bool Registry::HasComponent(Entity entity) {
+  const auto componentId = Component<TComponent>::GetId();
+  const auto entityId = entity.GetId();
+
+  return this->_entityComponentSignatures[entityId].test(componentId);
+};
+
+template <typename TComponent> void Registry::RemoveComponent(Entity entity) {
+  const auto componentId = Component<TComponent>::GetId();
+  const auto entityId = entity.GetId();
+
+  this->_entityComponentSignatures[entityId].set(componentId, false);
 };
